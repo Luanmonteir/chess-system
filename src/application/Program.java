689c215cd7ce12a43b217098a7102c755921bb9e
Luanmonteiro <luandasnevesmonteirocardoso@gmail.com>
2025-03-27 14:19:22 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,7 +43,11 @@ public class Program {
 
                 if (chessMatch.getPromoted()!=null){
                     System.out.print("Enter piece For Promotion (B/N/R/Q): ");
-                    String type= sc.nextLine();
+                    String type= sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N")&&!type.equals("R")&& !type.equals("Q")){
+                        System.out.print("Invalid Value ! Enter piece For promotion (B/N/R/Q):");
+                        type= sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
